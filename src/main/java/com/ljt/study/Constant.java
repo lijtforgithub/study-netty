7715c4255a9d03137a271e6ece477e8477b17ac9
lijtforgithub <lijtforjava@sina.com>
2020-04-29 11:48:48 +0800

    Hello World
    Hello World

---
+++ src/main/java/com/ljt/study/Constant.java
@@ -0,0 +1,13 @@
+package com.ljt.study;
+
+/**
+ * @author LiJingTang
+ * @date 2020-04-29 08:57
+ */
+public class Constant {
+
+    private Constant() {}
+
+    public static final String LOCAL_HOST = "localhost";
+
+}
--- src/main/java/com/ljt/study/netty/HelloWord.java
+++ src/main/java/com/ljt/study/netty/HelloWord.java
@@ -1,21 +1,55 @@
 package com.ljt.study.netty;
 
+import com.ljt.study.util.ExecutorUtils;
+import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.task.TaskExecutor;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+
+import static com.ljt.study.Constant.LOCAL_HOST;
 
 /**
  * @author LiJingTang
  * @date 2020-04-28 17:56
  */
+@Slf4j
 public class HelloWord {
 
-    private static class Server {
+    public static void main(String[] args) {
+        int port = 8080;
+        Server server = new Server(port);
+        Client client = new Client(port, LOCAL_HOST);
+        TaskExecutor taskExecutor = ExecutorUtils.newExecutor(2);
+        taskExecutor.execute(server);
+        ExecutorUtils.sleepSeconds(1);
+        taskExecutor.execute(client);
 
-        public void bind(int port) throws Exception {
+        ExecutorUtils.sleepSeconds(5);
+        System.exit(0);
+    }
+
+    private static final String ORDER = "QUERY TIME ORDER";
+    private static final String BAD = "BAD ORDER";
+
+    @AllArgsConstructor
+    private static class Server implements Runnable {
+
+        private int port;
+
+        @Override
+        public void run() {
             // 配置服务端的NIO线程组
             EventLoopGroup bossGroup = new NioEventLoopGroup();
             EventLoopGroup workGroup = new NioEventLoopGroup();
@@ -25,11 +59,15 @@ public class HelloWord {
                 bootstrap.group(bossGroup, workGroup)
                         .channel(NioServerSocketChannel.class)
                         .option(ChannelOption.SO_BACKLOG, 1024)
-                        .childHandler();
+                        .childHandler(new ChildChannelHandler());
                 // 绑定端口 同步等待成功
                 ChannelFuture future = bootstrap.bind(port).sync();
+                log.info("服务端启动成功");
                 // 等待服务端口监听端口关闭
                 future.channel().closeFuture().sync();
+                log.info("服务端关闭");
+            } catch (InterruptedException e) {
+                log.error(StringUtils.EMPTY, e);
             } finally {
                 // 优雅退出 释放线程池资源
                 bossGroup.shutdownGracefully();
@@ -41,29 +79,101 @@ public class HelloWord {
     private static class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
 
         @Override
-        protected void initChannel(SocketChannel socketChannel) throws Exception {
+        protected void initChannel(SocketChannel socketChannel) {
             socketChannel.pipeline().addLast(new TimeServerHandler());
 
         }
     }
 
+    @Slf4j
     private static class TimeServerHandler extends ChannelHandlerAdapter {
 
         @Override
-        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        public void channelRead(ChannelHandlerContext ctx, Object msg) {
             ByteBuf byteBuf = (ByteBuf) msg;
             byte[] req = new byte[byteBuf.readableBytes()];
-            
+            byteBuf.readBytes(req);
+            String body = new String(req);
+            log.info("服务端接收到数据：{}", body);
+
+            String currentTime = ORDER.equalsIgnoreCase(body) ? LocalDateTime.now().toString() : BAD;
+            ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes(StandardCharsets.UTF_8));
+            ctx.write(resp);
         }
 
         @Override
-        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        public void channelReadComplete(ChannelHandlerContext ctx) {
             ctx.flush();
         }
 
         @Override
-        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
             ctx.close();
+        }
+    }
+
+    @AllArgsConstructor
+    private static class Client implements Runnable {
+
+        private int port;
+        private String host;
+
+        @Override
+        public void run() {
+            // 配置客户端NIO线程组
+            EventLoopGroup group = new NioEventLoopGroup();
+
+            try {
+                Bootstrap bootstrap = new Bootstrap();
+                bootstrap.group(group)
+                        .channel(NioSocketChannel.class)
+                        .option(ChannelOption.TCP_NODELAY, true)
+                        .handler(new ChannelInitializer<SocketChannel>() {
+                            @Override
+                            protected void initChannel(SocketChannel ch) {
+                                ch.pipeline().addLast(new TimeClientHandler());
+                            }
+                        });
+
+                // 发起异步连接操作
+                ChannelFuture future = bootstrap.connect(host, port).sync();
+                log.info("客户端启动成功");
+                // 等待客户端链路关闭
+                future.channel().closeFuture().sync();
+                log.info("客户端关闭");
+            } catch (InterruptedException e) {
+                log.error(StringUtils.EMPTY, e);
+            } finally {
+                group.shutdownGracefully();
+            }
+        }
+    }
+
+    @Slf4j
+    private static class TimeClientHandler extends ChannelHandlerAdapter {
+
+        private final ByteBuf firstMessage;
+
+        public TimeClientHandler() {
+            byte[] req = ORDER.getBytes();
+            firstMessage = Unpooled.buffer(req.length);
+            firstMessage.writeBytes(req);
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+            log.error(StringUtils.EMPTY, cause);
+        }
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) {
+            ctx.writeAndFlush(firstMessage);
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) {
+            ByteBuf byteBuf = (ByteBuf) msg;
+            log.info("客户端接收到数据：{}", byteBuf.toString(StandardCharsets.UTF_8));
         }
     }
 
